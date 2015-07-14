0ba33f4d87d86793cf2c434a314694976760c7d3
Tom Hensel <tom@jitter.eu>
2015-07-14 11:37:26 +0200

    fix logic

--- peco.fish
+++ peco.fish
@@ -1,9 +1,7 @@
 function init --on-event init_peco
   set -l peco_bin (which peco)
-
-  if not set -q $peco_bin
-    echo "⚫  Please install 'peco' first:"
-    echo "⚫    brew install peco"
+  if not test -n "$peco_bin"
+    echo "⚫  Please install 'peco' first: brew install peco"
   else
     function peco_select_history
       history|peco|read slct
